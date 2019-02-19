9d1da376330b5b6f3e53eb3a90c54737a760a230
supriya <supriya.s363@gmail.com>
2019-02-19 21:23:40 +0530

    finding key, extra funcs added

--- inspect.py
+++ inspect.py
@@ -8,8 +8,6 @@ end_bit = 6
 
 def output_possibilities(output_xor):  
 	output_pairs = {}  # {[0,0,0,0]:[0,0,0,0] , [1,0,0,1]: [1,0,1,0]}
-
-
 	pass
 
 
@@ -18,7 +16,7 @@ def input_xor_possibilities(input_xor, output_pairs):
 	input_pairs = {}
 	pass
 
-def find_key_possibilities(input_pairs, r5): #SUP
+def find_key_possibilities(input_pairs, r5): 
 	key_set = []
 	for u1 in input_pairs.keys():
 		key_set.append(xor(u1, r5[start_bit:end_bit]))
@@ -28,12 +26,12 @@ def inspect_possibilities(input_xor, output_xor, r5):  #all are bit arrays
 
 	output_pairs = output_possibilities(output_xor)
 	input_pairs = input_possibilities(input_xor, output_pairs)
-	key_set = find_key_possibilities(input_pairs, r5) #r5 to be passed
+	key_set = find_key_possibilities(input_pairs, r5) 
 	pass
 
-def start_inspecting(r6xor, r5xor, l5xor, r5): #DONE
+def start_inspecting(r6xor, r5xor, l5xor, r5): 
 	permutation_output = xor(l5xor, r6xor)
-	output_xor = inverse_permute(permutation_output) #DONE
+	output_xor = inverse_permute(permutation_output) 
 	input_xor = expansion(r5xor)
 	actual_input = expansion(r5)
 	inspect_possibilities(input_xor, output_xor, actual_input)
