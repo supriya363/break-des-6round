f74b56102fbb9d0d7b26d4b226ce050cbfecf0bd
supriya <supriya.s363@gmail.com>
2019-02-25 19:51:27 +0530

    fixed find_key

--- find_key.py
+++ find_key.py
@@ -2,6 +2,31 @@
 from des import *
 import os
 import sys
+from collections import Counter
+
+def getFrequencyOfKeys(thirtybit_keylist):
+    # count = 0
+    # thirty_bit_key = []
+    # for key0 in keys_of_sboxes[0]:
+    #     for key1 in keys_of_sboxes[1]:
+    #         for key2 in keys_of_sboxes[2]:
+    #             for key3 in keys_of_sboxes[3]:
+    #                 for key4 in keys_of_sboxes[4]:
+    #                     thiry_bits_key_str = key0 + key1 + key2 + key3 + key4;
+    #                     thirty_bit_key.append(thiry_bits_key_str);
+    #                     count += 1;
+    frequency_of_key = Counter(thirty_bit_key)
+    return frequency_of_key;
+
+def xor(input1, input2):
+	res = []
+	for i in range(len(input1)):
+		xorval = (ord(input1[i])-ord('0'))^(ord(input2[i])-ord('0'))
+		res.append(chr(xorval+ord('0')))
+	return res
+
+def convBitList2Int(bitList):
+    return int("".join(str(i) for i in bitList),2)
 
 def output_possibilities(output_xor):  
 	output_pairs = {}  # {[0,0,0,0]:[0,0,0,0] , [1,0,0,1]: [1,0,1,0]}
@@ -12,6 +37,14 @@ def output_possibilities(output_xor):
 		output_pairs[convBitList2Int(input2)] = [input1,input2]
 	return output_pairs
 
+def make_key_list():	# Returns list of all possible 64 keys[6 bits]
+	key_list=[]
+	for i in range(64):
+		tmp=bin(i)[2:]
+		while len(tmp) < 6:
+			tmp = "0" + tmp
+		key_list.append([tmp,0])
+	return key_list
 
 def get_sbox(sbox):
 	if sbox == 1:
@@ -78,62 +111,72 @@ def divide_input_and_output():
 	if os.path.isfile('inputxor.txt') and os.path.isfile('outputxor.txt'):
 		f_inp = open('inputxor.txt','r')
 		f_out = open('outputxor.txt','r')
-		f_r5 = open('expandr3.txt','r')
+		f_r5 = open('expandr5.txt','r')
 		
 		inputxor_list = []
 		outputxor_list = []
-		# count = 1
+		count = 1
 		for line_inp, line_out, line_r5 in zip(f_inp,f_out, f_r5):
 			j = 0
 			inputxor_list = [ line_inp[i:i+6] for i in range(0,len(line_inp)-1,6)]
 			outputxor_list = [ line_out[j:j+4] for j in range(0,len(line_out)-1,4)]
-			# print(inputxor_list)
-			# print(outputxor_list)
 			r5 = line_r5[:48]
-			#print(r5)
-			
+			sbox_keylist = []
 			sbox = 1
 			for input_xor,output_xor in zip(inputxor_list,outputxor_list):
-				# print(sbox)
-				if sbox !=1 or sbox!= 3 or sbox!= 4:
+				
+				if sbox != 1 and sbox != 3 and sbox != 4:
+					# print(sbox)
 					output_pairs = output_possibilities(output_xor)
 					input_pairs = input_xor_possibilities(input_xor, output_pairs, sbox)
-					# print(len(input_pairs))
-
-					# if count < 10 :
-						# print(count)
-					find_key_possibilities(input_pairs, r5, sbox)
-
-					# count+=1
+					if count < 6:
+						print(count)
+						sbox_keylist = find_key_possibilities(input_pairs, r5, sbox, sbox_keylist)
+					count+=1
 				sbox+=1
-
 	else:
 		print("No input file found here\n")
 		raise SystemExit
 
-def make_key_list():	# Returns list of all possible 64 keys[6 bits]
-	key_list=[]
-	for i in range(64):
-		tmp=bin(i)[2:]
-		while len(tmp) < 6:
-			tmp = "0" + tmp
-		key_list.append([tmp,0])
-	return key_list
+def findKey30(keys_of_sboxes):
+	count = 0
+	thirty_bit_key = []
+	for key0 in keys_of_sboxes[0]:
+		for key1 in keys_of_sboxes[1]:
+			for key2 in keys_of_sboxes[2]:
+				for key3 in keys_of_sboxes[3]:
+					for key4 in keys_of_sboxes[4]:
+						thiry_bits_key_str = key0 + key1 + key2 + key3 + key4;
+						thirty_bit_key.append(thiry_bits_key_str);
+						count += 1;
+	return thirty_bit_key
 
-def find_key_possibilities(input_pairs, r5, sbox):
+def find_key_possibilities(input_pairs, r5, sbox, sbox_keylist):
 	f_key = open('keyset'+str(sbox)+'.txt','w+')
 	print(len(input_pairs))
+
+	res = []
 	for i in range(len(input_pairs)):
-		key_list[sbox-1][int(''.join(xor(input_pairs[i][0], r5[(sbox-1)*6:(sbox-1)*6+6])),2)][1] += 1	# xor of 6 bits of key and corresponding S-box input
-		print(key_list[sbox-1][int(''.join(xor(input_pairs[i][0], r5[(sbox-1)*6:(sbox-1)*6+6])),2)][0])
-	for i in range(64):
-		f_key.write(str(key_list[sbox-1][i][0])+':'+str(key_list[sbox-1][i][1]))
-		f_key.write("\n")
-	f_key.close()				
+		res.append(''.join(xor(input_pairs[i][0], r5[(sbox-1)*6:(sbox-1)*6+6])))
+	print(res)		# res stores 6 bit key possibilty for 'sbox' passed 
+	sbox_keylist.append(res)		# list having possible keys from all 5 sbox 
+	print("\n")
+	print(sbox_keylist)
 
+	if sbox == 8:
+		tmp_list = findKey30(sbox_keylist)
+		for i in tmp_list:
+			thirtybit_keylist.append(i)		# In thirtybit_keylist count the frequency 
+	f_key.close()
+	return sbox_keylist				
 
+thirtybit_keylist = []
 key_list = []
 for i in range(8):
 	key_list.append(make_key_list())
 
 divide_input_and_output()
+
+# frequency_of_keys.most_common()
+# frequency_of_keys = getFrequencyOfKeys(thirtybit_keylist)
+# print(frequency_of_keys)
